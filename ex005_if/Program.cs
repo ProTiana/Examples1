d49f983c256f899568cf330d14196f0cb67e41c0
protiana <protiana@yandex.ru>
2022-08-14 15:06:43 +0300

    особое приветствие для tati

---
+++ ex005_if/Program.cs
@@ -0,0 +1,13 @@
+﻿// Приветсивие  по условию
+Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "tati")
+{
+    Console.WriteLine("Yep!! Tati is here");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
