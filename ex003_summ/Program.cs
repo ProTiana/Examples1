c19d2e1e0e2cebe56ef13e387c47b55678d64f49
protiana <protiana@yandex.ru>
2022-08-14 14:30:05 +0300

    random sum

--- ex003_summ/Program.cs
+++ ex003_summ/Program.cs
@@ -1,6 +1,8 @@
 ﻿// сложение 2х чисел
-int numberA = 50;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); //  случайное число в диапазоне 123...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
